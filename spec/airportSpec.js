2e5b92b5e9e394e62b282b220c69be5c60795e25
Vicky Gray <vickygray26@hotmail.com>
2016-02-16 17:50:19 +0000

    prevents take off and landing when weather stormy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -6,13 +6,13 @@ describe("Airport", function() {
 
   var plane;
   var airport;
-  // var capacity = airport.DEFAULTCAPACITY;
 
   beforeEach(function() {
     airport = new Airport();
   });
 
   it("instructs a plane to land and confirms has landed", function() {
+    spyOn(airport, 'isStormy').and.returnValue(false);
     airport.land(plane);
     expect(airport.landedPlanes).toContain(plane);
   });
@@ -22,6 +22,7 @@ describe("Airport", function() {
   // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 
   it("instructs a plane to take off and confirms has left the airport", function() {
+    spyOn(airport, 'isStormy').and.returnValue(false);
     airport.takeOff(plane);
     expect(airport.landedPlanes).not.toContain(plane);
   });
@@ -31,6 +32,7 @@ describe("Airport", function() {
   // I want to prevent landing when the airport is full
 
   it("prevents a plane from landing when aiport full", function() {
+    spyOn(airport, 'isStormy').and.returnValue(false);
     for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
       airport.land(plane);
     }
@@ -42,6 +44,7 @@ describe("Airport", function() {
   // I would like a default airport capacity that can be overridden as appropriate
 
   it("can override the DEFAULTCAPACITY", function() {
+    spyOn(airport, 'isStormy').and.returnValue(false);
     airport.DEFAULTCAPACITY = 30;
     expect(airport.DEFAULTCAPACITY).toEqual (30);
     for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
@@ -54,13 +57,16 @@ describe("Airport", function() {
   // To ensure safety
   // I want to prevent takeoff when weather is stormy
   it("prevents take off when weather is stormy", function() {
-    // stub random weather function
+    spyOn(airport, 'isStormy').and.returnValue(true);
     expect( function(){airport.takeOff(plane); } ).toThrow("It's raining men! Hallelujah! (But you can't take off.)");
   });
 
+  // As an air traffic controller
+  // To ensure safety
+  // I want to prevent landing when weather is stormy
+  it("prevents landing when weather is stormy", function() {
+    spyOn(airport, 'isStormy').and.returnValue(true);
+    expect( function(){airport.land(plane); } ).toThrow("It's raining men! Hallelujah! (But you can't land.)");
+  });
+
 });
-
-
-// As an air traffic controller
-// To ensure safety
-// I want to prevent landing when weather is stormy
--- src/airport.js
+++ src/airport.js
@@ -6,6 +6,9 @@ function Airport()  {
 Airport.prototype.land = function(plane) {
   if (this.landedPlanes.length >= this.DEFAULTCAPACITY)
     throw "Airport is full, jog on";
+  else if (this.isStormy()) {
+    throw "It's raining men! Hallelujah! (But you can't land.)";
+  }
   else {
     this.landedPlanes.push(plane);
   }
