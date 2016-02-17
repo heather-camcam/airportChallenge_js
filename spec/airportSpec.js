1d8ea4864102c3378bfb735170a83f415be50134
Heather <campbell.hf@gmail.com>
2016-02-17 10:38:08 +0000

    refactored spec file to split out good weather spy into before block

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -11,47 +11,51 @@ describe("Airport", function() {
     airport = new Airport();
   });
 
-  it("instructs a plane to land and confirms has landed", function() {
-    spyOn(airport, 'isStormy').and.returnValue(false);
-    airport.land(plane);
-    expect(airport.landedPlanes).toContain(plane);
-  });
+  describe("Good weather", function () {
 
-  // As an air traffic controller
-  // So I can get passengers on the way to their destination
-  // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+    beforeEach(function() {
+      spyOn(airport, 'isStormy').and.returnValue(false);
+    });
 
-  it("instructs a plane to take off and confirms has left the airport", function() {
-    spyOn(airport, 'isStormy').and.returnValue(false);
-    airport.takeOff(plane);
-    expect(airport.landedPlanes).not.toContain(plane);
-  });
-
-  // As an air traffic controller
-  // To ensure safety
-  // I want to prevent landing when the airport is full
-
-  it("prevents a plane from landing when aiport full", function() {
-    spyOn(airport, 'isStormy').and.returnValue(false);
-    for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
+    it("instructs a plane to land and confirms has landed", function() {
       airport.land(plane);
-    }
-    expect( function(){airport.land(plane); } ).toThrow("Airport is full, jog on");
+      expect(airport.landedPlanes).toContain(plane);
+    });
+
+    // As an air traffic controller
+    // So I can get passengers on the way to their destination
+    // I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
+
+    it("instructs a plane to take off and confirms has left the airport", function() {
+      airport.takeOff(plane);
+      expect(airport.landedPlanes).not.toContain(plane);
+    });
+
+    // As an air traffic controller
+    // To ensure safety
+    // I want to prevent landing when the airport is full
+
+    it("prevents a plane from landing when aiport full", function() {
+      for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
+        airport.land(plane);
+      }
+      expect( function(){airport.land(plane); } ).toThrow("Airport is full, jog on");
+    });
+
+    // As the system designer
+    // So that the software can be used for many different airports
+    // I would like a default airport capacity that can be overridden as appropriate
+
+    it("can override the DEFAULTCAPACITY", function() {
+      airport.DEFAULTCAPACITY = 30;
+      expect(airport.DEFAULTCAPACITY).toEqual (30);
+      for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
+        airport.land(plane);
+      }
+      expect(airport.landedPlanes.length).toEqual (30);
+    });
   });
 
-  // As the system designer
-  // So that the software can be used for many different airports
-  // I would like a default airport capacity that can be overridden as appropriate
-
-  it("can override the DEFAULTCAPACITY", function() {
-    spyOn(airport, 'isStormy').and.returnValue(false);
-    airport.DEFAULTCAPACITY = 30;
-    expect(airport.DEFAULTCAPACITY).toEqual (30);
-    for(i = 0; i < (airport.DEFAULTCAPACITY); i ++) {
-      airport.land(plane);
-    }
-    expect(airport.landedPlanes.length).toEqual (30);
-  });
 
   // As an air traffic controller
   // To ensure safety
